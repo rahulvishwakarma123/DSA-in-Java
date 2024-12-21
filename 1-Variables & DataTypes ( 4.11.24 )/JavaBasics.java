5902a13dca0632a87457a4f58303b60a60bc5deb
Rahul Vishwakarma <vishwa.rahul638@gmail.com>
2024-12-21 16:48:06 +0530

    1-Variables & DataTypes

---
+++ 1-Variables & DataTypes ( 4.11.24 )/JavaBasics.java
@@ -0,0 +1,6 @@
+public class JavaBasics{
+    public static void main(String arg[]){
+        System.out.println("Hello world!");
+        
+    }
+}
---
+++ 1-Variables & DataTypes ( 4.11.24 )/typeCasting.java
@@ -0,0 +1,8 @@
+public class typeCasting {
+    public static void main(String[] args) {
+        float marks = 99.999f;
+        int a = (int)marks;
+        System.out.println(a);
+        
+    }
+}
