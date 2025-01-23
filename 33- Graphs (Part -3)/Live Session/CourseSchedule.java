5225d0391c0ddd14c2189d0a2169fd01ed7d7d5b
Rahul Vishwakarma <vishwa.rahul638@gmail.com>
2025-01-23 21:28:12 +0530

    added course schedule added.

---
+++ 33- Graphs (Part -3)/Live Session/CourseSchedule.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class CourseSchedule {
+    public boolean canFinish(int n, int[][] pre) 
+    {
+         ArrayList<ArrayList<Integer>>adj= new ArrayList<>();
+         int indegree[]= new int[n];
+         for( int i=0;i<n;i++)
+         {
+              // adj[0]=[]
+              // adj[1]=[]
+              adj.add( new ArrayList<Integer>());
+         }
+         for( int i=0;i< pre.length;i++)
+         {
+              int a= pre[i][0];
+              int b= pre[i][1];
+              // b->a
+              adj.get(b).add( a );
+              indegree[a]++;
+ 
+         }
+ 
+         Queue<Integer>q= new LinkedList<>();
+         for( int i=0;i<n;i++)
+         {
+             if( indegree[i]==0)
+             {
+                 q.add( i );
+             }
+         }
+        ArrayList<Integer>ans= new ArrayList<>();
+        while(q.isEmpty()==false)
+        {
+             int curr= q.remove();
+             ans.add( curr );
+ 
+             for( int conn : adj.get(curr) )
+             {
+                 indegree[conn]--;
+                 if( indegree[conn]==0)
+                 {
+                     q.add( conn );
+                 }
+             }
+        }
+ 
+        if( ans.size()==n) //all courses are in ans
+        {
+            return true; // its possible to do all courses
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
