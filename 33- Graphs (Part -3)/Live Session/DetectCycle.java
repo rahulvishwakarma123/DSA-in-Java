68aabb5ac2f4e9673a2bcd0bb9c0d59f0bd56571
Rahul Vishwakarma <vishwa.rahul638@gmail.com>
2025-01-23 21:11:22 +0530

    detect cycle code added.

---
+++ 33- Graphs (Part -3)/Live Session/DetectCycle.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class DetectCycle {
+    static  boolean detectCycleInDirectedGraph( ArrayList<ArrayList<Integer>>adj )
+    {
+           int n = adj.size();
+           int indegree[]= new int[n];
+           for( int curr=0;curr<n; curr++)
+           {
+               for( int conn : adj.get(curr) )
+               {
+                    indegree[conn]++;
+               }
+           }
+           Queue<Integer>q= new LinkedList<>();
+           for( int i=0;i<n;i++)
+           {
+              if( indegree[i]==0)
+              {
+                  q.add( i );
+              }
+           }
+           ArrayList<Integer>ans= new ArrayList<>();
+           while( q.isEmpty()==false)
+           {
+               int curr= q.remove();
+               ans.add( curr );
+               for( int conn : adj.get(curr) )
+               {
+                    indegree[conn]--;
+                    if(indegree[conn]==0)
+                    {
+                        q.add(conn);
+                    }
+               }
+           }
+           if( ans.size()==n) // no cycle
+           {
+             return false;
+           }
+           else // cycle found
+           {
+             return true;
+           }
+ 
+    }
+}
