d59c20fd5533ad45fe1a324dc56430943aeae6fb
Rahul Vishwakarma <vishwa.rahul638@gmail.com>
2025-01-24 11:32:39 +0530

    Added the codde minimum sum of absolute value pairs.

--- 22- Greedy Algorithms/Lecture/MinAbsoluteDiffrencePairs.java
+++ 22- Greedy Algorithms/Lecture/MinAbsoluteDiffrencePairs.java
@@ -1,5 +1,19 @@
+import java.util.*;
 public class MinAbsoluteDiffrencePairs {
     public static void main(String[] args) {
+        int a[] = {4,1,8,7};
+        int b[] = {2,3,6,5};
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        int minAbsVal = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            minAbsVal += Math.abs(a[i] - b[i]);
+        }
+
+        System.out.println("Minimum absolute value = " + minAbsVal);
         
     }
 }
