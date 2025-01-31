3b738b5645dd603a2f1d0c07ff7d400e47f221ae
Rahul Vishwakarma <vishwa.rahul638@gmail.com>
2025-01-31 18:46:52 +0530

    Added the code of Climbing stairs of DP.

--- 38- Dynamic Programming/Lecture/ClimbingStairs.java
+++ 38- Dynamic Programming/Lecture/ClimbingStairs.java
@@ -1,5 +1,20 @@
-
-
 public class ClimbingStairs {
-    
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arr = new int[n+1];
+        System.out.println(countWays(n, arr));
+    }
+    public static int countWays(int n, int[] arr) {
+        if (n == 1 || n == 0) {
+            return 1; 
+        }
+        else if(n < 0){
+            return 0;
+        }
+        if (arr[n] != 0) { 
+            return arr[n];
+        }
+        arr[n] = countWays(n-2, arr) + countWays(n-1, arr);
+        return arr[n];
+    }
 }
