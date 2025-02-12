108f1df38a6463ee0588139763eafdbdfa2d2af3
Rahul Vishwakarma <vishwa.rahul638@gmail.com>
2025-02-12 18:09:45 +0530

    Added the code of Longest common substring.

--- 40- DP (Part-4)/Lecture/LargestCommonSubString.java
+++ 40- DP (Part-4)/Lecture/LargestCommonSubString.java
@@ -1,39 +1,39 @@
 public class LargestCommonSubString {
     // A substring is a contiguous sequence of characters of a string
 
-    public static int rec(String str1, String str2, int n, int m, int[][] dp){
-        if (m <= 0 || n <= 0) {
-            return 0;
-        }
-        if (dp[n][m] != -1) {
-            return dp[n][m];
-        }
-        int ans;
-        if (str1.charAt(n-1) == str2.charAt(m-1)) {
-            ans = 1 + rec(str1, str2, n-1, m-1, dp);
-            dp[n][m] = ans;
-            return ans;
-        }
-        else {
-            ans = Math.max(rec(str1, str2, n-1, m, dp), rec(str1, str2, n, m-1, dp));
-            dp[n][m] = ans;
-            return ans;
-        }
-    }
-    public static void main(String[] args) {
-        String str1 = "ABCDE";
-        String str2 = "ABCDE";
-        int n = str1.length();
-        int m = str2.length();
+    public static int tabLCS(String s1, String s2, int n, int m){
         int[][] dp = new int[n+1][m+1];
 
         for (int i = 0; i < dp.length; i++) {
             for (int j = 0; j < dp.length; j++) {
-                dp[i][j] = -1;
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
             }
         }
+        int ans = 0;
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < m+1; j++) {
+                if (s1.charAt(i-1) == s2.charAt(j-1) ) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    ans = Math.max(ans, dp[i][j]);
+                }
+                else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return ans;
+
+    }
+    public static void main(String[] args) {
+        String str1 = "ABCDE";
+        String str2 = "ABCCE";
+        int n = str1.length();
+        int m = str2.length();
+        
         // int[] arr = new int[n];
-        int ans = rec(str1, str2, n, m, dp);
+        int ans = tabLCS(str1, str2, n, m);
         System.out.println(ans);
         
     }
