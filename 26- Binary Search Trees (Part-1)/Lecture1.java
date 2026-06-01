9365746bb949444546c9469543dd3b53205943ac
Rahul Vishwakarma <vishwa.rahul638@gmail.com>
2026-06-01 17:52:56 +0530

    added the bst 2 lecture code.

--- 26- Binary Search Trees (Part-1)/Lecture1.java
+++ 26- Binary Search Trees (Part-1)/Lecture1.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Lecture1 {
         static class Node {
@@ -10,6 +11,8 @@ public class Lecture1 {
             }
         }
 
+        //  ========================================================================================================================       
+
         // for inserting the Node into the bst;
         public static Node insert(Node root, int val){
             if(root == null){
@@ -17,13 +20,16 @@ public class Lecture1 {
                 root = newNode;
                 return root;
             }
-            if(root.val > val){
-                root.left = insert(root.left, val);
-            }else
+            if(root.val < val){
                 root.right = insert(root.right, val);
+            }else
+                root.left = insert(root.left, val);
             return root;
         }
-
+        
+        
+        
+        //  ========================================================================================================================       
         // print the inOrder sequence 
         public static void inOrder(Node root){
             if(root == null) return;
@@ -31,7 +37,8 @@ public class Lecture1 {
             System.out.print(root.val);
             inOrder(root.right);
         }
-
+        
+        //  ========================================================================================================================       
         // for searching the Node from the bst;
         public static Node search(Node root, int val){
             if(root == null){
@@ -41,7 +48,8 @@ public class Lecture1 {
             else if(root.val < val) return search(root.right, val);
             else return root;
         }
-
+        
+        //  ========================================================================================================================       
         // function for deleting a Node from the Tree;
         public static Node delete(Node root, int data){
             if(root == null) return null;
@@ -71,6 +79,8 @@ public class Lecture1 {
             }
             return root;
         }
+
+        // inorder successor is left most node in right subtree.
         public static Node findIS(Node root){
             while (root.left != null) {
                 root = root.left;
@@ -78,17 +88,60 @@ public class Lecture1 {
             return root;
         }
 
+        // ==================================================================================================
+        // printing range sum of bst
+        // public static int sum = 0;
+        // public static int rangeSum(Node root, int min, int max){
+        //     if(root == null) return -1;
+        //     if(root.val >= min) rangeSum(root.left, min, max);
+        //     sum += root.val;
+        //     if(root.val <= max) rangeSum(root.right, min, max);
+        //     return sum;
+        // }
+
+
+        // ==========================================================================================================
+        // print all paths from root to leaf
+        public static void printPaths(Node root, List<Node> list){
+            if(root == null) return;
+            // this is leaf node condition
+            if(root.left == null && root.right == null){
+                list.add(root);
+                for (Node node : list) {
+                    System.out.print(node.val);   
+                }  
+                System.out.println();
+                // we have to remove the leaf nodes from the list because all recursive calls share the same space of objects.
+                list.removeLast();
+                return;           
+            }
+            
+            list.add(root);
+            printPaths(root.left, list);
+            printPaths(root.right, list);
+
+            // we have to remove the root for the next roots
+            list.removeLast();
+        }
+
+
+    // ==================================================================================================
     public static void main(String[] args) {
-        int[] values = {2,3,5,2,5,2,7};
+        int[] nodeValues = {2,3,5,2,5,2,7};
         Node root = null;
-        for (int val : values) {
+        for (int val : nodeValues) {
             root = insert(root, val);
         }
         Node val = search(root, 5);
         System.out.println(val.val);
-        delete(root, 3);
+        // delete(root, 3);
         inOrder(root);
-
-
+        // rangeSum(root, 2, 3);
+        // System.out.println("");
+        
+        // System.out.println(sum);
+        System.out.println("Here is your all paths from the root to leaf.");
+        printPaths(root, new ArrayList<>());
+        
     }
 }
--- 27- Binary Search Trees (Part-2)/BST2.java
+++ 27- Binary Search Trees (Part-2)/BST2.java
@@ -1,21 +1,24 @@
-public class BST2{
-    class BST{
-        class Node {
-            int val;
-            Node left;
-            Node right;
-            public Node(int v){
-                this.left = null;
-                this.right = null;
-                this.val = v;
-            }
-        }
-        void add();
-        Node delete();
-        Node search()
 
+public class BST2 {
+
+    static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        Node(int v) {
+            this.val = v;
+        }
     }
+
+    // ===============================================================
+    // making the bst with sorted array with minimum height
+    // public static Node buildBST(Node root, int arr){
+
+    // }
+
     public static void main(String[] args) {
-        
+        int arr[] = {1,5,6,8,10,11,12};
+
     }
 }
